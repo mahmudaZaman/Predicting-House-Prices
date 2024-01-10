da7acbec3867426d795fb56eaca84817d82b8c7d
shuchi <mahmudazaman0509@gmail.com>
2024-01-10 19:00:01 +0800

    fix file path

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pickle
 import numpy as np
 
 # import the model
-pipe = pickle.load(open("../house_price_prediction/artifacts/model.pkl","rb"))
-df = pickle.load(open('../house_price_prediction/artifacts/data.pkl','rb'))
+pipe = pickle.load(open("artifacts/model.pkl", "rb"))
+df = pickle.load(open('artifacts/data.pkl', 'rb'))
 
 st.title("House Price Predictor")
 
@@ -15,9 +15,9 @@ lot = st.number_input('Lot')
 tax = st.number_input('Tax need to pay')
 bathroom = st.number_input('Number of bathrooms')
 garage = st.number_input('Number of garage')
-condition = st.selectbox('condition',df['Condition'].unique())
+condition = st.selectbox('condition', df['Condition'].unique())
 
 if st.button('Predict Price'):
-    query = np.array([bedroom,space,room,lot,tax,bathroom,garage,condition])
-    query = query.reshape(1,8)
+    query = np.array([bedroom, space, room, lot, tax, bathroom, garage, condition])
+    query = query.reshape(1, 8)
     st.title("The predicted price of this house is " + str(int(pipe.predict(query)[0])))
